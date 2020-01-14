b3f9d6a12ca98e364c57acb53bac5e7c66dcfa9c
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-01-14 12:03:08 +0800

    ++lesson

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/testjavascript.js
@@ -0,0 +1,14 @@
+var a;
+var b = 2;
+var myName = "adhamaa";
+console.log(a);
+a = 7;
+
+b = a;
+
+console.log(a);
+console.log(myName);
+
+background(186, 145, 20); // wooden table
+ellipse(200, 200, 350, 350); // plate
+ellipse(200, 200, 300, 300);
