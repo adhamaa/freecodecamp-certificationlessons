d52d1b56a18e1435bbebf5dadd33099fca4f3c2e
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-01-17 22:24:51 +0800

    commit

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/testjavascript.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/testjavascript.js
@@ -1,15 +1,25 @@
-var a;
-var b = 2;
-var myName = "adhamaa";
-console.log(a);
-a = 7;
+const stringCounter = () => {
+  let myNameLength = 0;
+  var myName = "adhama akmal bin azmi bin abdul rahman";
 
-b = a;
+  myNameLength = myName.length;
+  console.log(myName.length);
 
-console.log(a);
-console.log(myName);
+  const updateCounter = () => {
+    const counter = document.querySelector(".counter");
+    counter.textContent = myNameLength;
+  };
+  //Execute Counter Update
+  updateCounter();
 
-var x = 5;
-var y = 10;
-
-console.log(x + y);
+  const updateName = () => {
+    const input = document.querySelector(".inputField");
+    const addTextBtn = document.querySelector(".addTextBtn");
+    input.value = "type to count";
+    console.log(input.value);
+  };
+  //Execute name
+  updateName();
+};
+//Execute main string counter
+stringCounter();
