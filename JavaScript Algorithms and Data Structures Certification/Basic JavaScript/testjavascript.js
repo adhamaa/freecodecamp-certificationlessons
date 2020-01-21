fb5542797537213f525b94a40020f0382d16ab9f
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-01-21 13:13:43 +0800

    update

--- JavaScript Algorithms and Data Structures Certification/Basic JavaScript/testjavascript.js
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/testjavascript.js
@@ -26,3 +26,36 @@ const stringCounter = () => {
 };
 //Execute main string counter
 stringCounter();
+
+function sequentialSizes(val) {
+  var answer = "";
+  switch (val) {
+    case 1:
+    case 2:
+    case 3:
+      answer = "Low";
+      break;
+    case 4:
+    case 5:
+    case 6:
+      answer = "Mid";
+      break;
+    case 7:
+    case 8:
+    case 9:
+      answer = "High";
+      break;
+  }
+  return answer;
+}
+
+// Change this value to test
+console.log(sequentialSizes(4));
+
+function isLess(a, b) {
+  // Fix this code
+  return a < b;
+}
+
+// Change these values to test
+console.log(isLess(10, 15));
