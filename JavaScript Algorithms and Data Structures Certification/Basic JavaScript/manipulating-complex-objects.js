ad25feb0baf7a3440a7c1997fb36357ce19af353
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-01-22 00:06:55 +0800

    up

---
+++ JavaScript Algorithms and Data Structures Certification/Basic JavaScript/manipulating-complex-objects.js
@@ -0,0 +1,25 @@
+var myMusic = [
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  },
+  // Add record here
+  {
+    "artist": "Billy Joel",
+    "title": "Piano Man",
+    "release_year": 1973,
+    "formats": [
+      "CD",
+      "8T",
+      "LP"
+    ],
+    "gold": true
+  }
+];
