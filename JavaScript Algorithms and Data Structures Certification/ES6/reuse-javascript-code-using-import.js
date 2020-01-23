52501321514da691b11361b991ac6a0e7fe4d984
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-01-23 21:36:15 +0800

    done ES6

---
+++ JavaScript Algorithms and Data Structures Certification/ES6/reuse-javascript-code-using-import.js
@@ -0,0 +1,6 @@
+import { uppercaseString, lowercaseString } from './string_functions.js';
+// add code above this line
+
+const up = uppercaseString("hello");
+const low = lowercaseString("WORLD!");
+console.log(up, low);
