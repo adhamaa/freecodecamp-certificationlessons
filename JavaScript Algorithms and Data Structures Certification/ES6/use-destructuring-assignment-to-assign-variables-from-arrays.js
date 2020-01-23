b71269371bd6959666c8f748e06c5ca677307504
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-01-23 20:59:12 +0800

    up commit

---
+++ JavaScript Algorithms and Data Structures Certification/ES6/use-destructuring-assignment-to-assign-variables-from-arrays.js
@@ -0,0 +1,7 @@
+let a = 8, b = 6;
+// change code below this line
+[a, b] = [b, a];
+// const [,,,,, a,, b] = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// change code above this line
+console.log(a); // should be 6
+console.log(b); // should be 8
