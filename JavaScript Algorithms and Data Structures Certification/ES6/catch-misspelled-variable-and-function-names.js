7d20f593144f2bf1a9a095a3fa12ef18d6a8ecbe
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-01-25 01:14:23 +0800

    regex done, debugging and data structures

---
+++ JavaScript Algorithms and Data Structures Certification/ES6/catch-misspelled-variable-and-function-names.js
@@ -0,0 +1,4 @@
+let receivables = 10;
+let payables = 8;
+let netWorkingCapital = receivables - payables;
+console.log(netWorkingCapital);
---
+++ JavaScript Algorithms and Data Structures Certification/ES6/check-for-mixed-grouping-of-characters.js
@@ -0,0 +1,5 @@
+let myString = "Eleanor Roosevelt";
+let myRegex = /^(Franklin|Eleanor).*Roosevelt/; // Change this line
+let result = myRegex.test(myString); // Change this line
+// After passing the challenge experiment with myString and see how the grouping works
+console.log(result);
