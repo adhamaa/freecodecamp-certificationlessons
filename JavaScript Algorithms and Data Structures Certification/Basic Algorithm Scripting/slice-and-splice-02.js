8c2e8a69c4b845f59ebf26003c94a6f51613e455
adhamaa <adham_92@live.com>
2020-01-31 22:26:18 +0800

    chng dir

---
+++ JavaScript Algorithms and Data Structures Certification/Basic Algorithm Scripting/slice-and-splice-02.js
@@ -0,0 +1,8 @@
+function frankenSplice(arr1, arr2, n) {
+  // It's alive. It's alive!
+   let localArr = arr2.slice();
+  localArr.splice(n, 0, ...arr1);
+  return localArr;
+}
+
+frankenSplice([1, 2, 3], [4, 5, 6], 1);
