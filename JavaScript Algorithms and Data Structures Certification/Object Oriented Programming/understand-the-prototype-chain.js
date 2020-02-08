5e0ab939065c7c2f931c5d8fe80d93aa5d76b9d6
adhamaa <adham_92@live.com>
2020-02-08 19:51:25 +0800

    up

---
+++ JavaScript Algorithms and Data Structures Certification/Object Oriented Programming/understand-the-prototype-chain.js
@@ -0,0 +1,12 @@
+function Dog(name) {
+  this.name = name;
+}
+
+let beagle = new Dog("Snoopy");
+
+Dog.prototype.isPrototypeOf(beagle);  // => true
+
+// Fix the code below so that it evaluates to true
+// ???.isPrototypeOf(Dog.prototype);
+
+console.log(Object.prototype.isPrototypeOf(beagle));
