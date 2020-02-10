23793e1f4559ff211549735d59ff43b1a6c8c7a3
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-02-10 19:53:08 +0800

    up

---
+++ JavaScript Algorithms and Data Structures Certification/Functional Programming/pass-arguments-to-avoid-external-dependence-in-a-function.js
@@ -0,0 +1,12 @@
+// the global variable
+var fixedValue = 4;
+
+// Add your code below this line
+function incrementer (n) {
+return fixedValue + n;
+
+  // Add your code above this line
+}
+
+var newValue = incrementer(1); // Should equal 5
+console.log(fixedValue); // Should print 4
