f026493218dc6301db336e3897b44d89d4e9101c
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-03-07 04:44:04 +0800

    up

---
+++ JavaScript Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/dna-pairing-02.js
@@ -0,0 +1,15 @@
+function pairElement(str) {
+  //create object for pair lookup
+  var pairs = {
+    A: "T",
+    T: "A",
+    C: "G",
+    G: "C"
+  };
+  //split string into array of characters
+  var arr = str.split("");
+  //map character to array of character and matching pair
+  return arr.map(x => [x, pairs[x]]);
+}
+
+console.log(pairElement("GCG"));
