fff508be6b05a28a24b80d2ddcd54981838b2f0c
Adham Akmal Azmi <50027371+adhamaa@users.noreply.github.com>
2020-04-06 04:33:14 +0800

    up

---
+++ JavaScript Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/everything-be-true-01.js
@@ -0,0 +1,6 @@
+function truthCheck(collection, pre) {
+    return collection.every(element => element.hasOwnProperty(pre) && Boolean(element[pre]));
+}
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
---
+++ JavaScript Algorithms and Data Structures Certification/Intermediate Algorithm Scripting/everything-be-true-02.js
@@ -0,0 +1,7 @@
+function truthCheck(collection, pre) {
+  // Is everyone being true?
+  return collection.every(x => x[pre]);
+}
+
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy", "sex": "male"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
+console.log(truthCheck([{"user": "Tinky-Winky", "sex": "male"}, {"user": "Dipsy"}, {"user": "Laa-Laa", "sex": "female"}, {"user": "Po", "sex": "female"}], "sex"));
